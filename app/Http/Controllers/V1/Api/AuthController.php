f632a647fdfdab4abb12d75c1a6d3e2f7cef4877
Happeace304 <happeace304@gmail.com>
2019-03-18 10:44:25 +0700

    chuyen App/User thanh App/Models/User

--- app/Http/Controllers/V1/Api/AuthController.php
+++ app/Http/Controllers/V1/Api/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\V1\Api;
 
 use App\Http\Resources\UserResource;
-use App\User;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
