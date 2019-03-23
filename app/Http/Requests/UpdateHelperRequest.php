d57e26fffe29fb0411afff97a616252ae59b8809
Happeace304 <happeace304@gmail.com>
2019-03-23 16:02:40 +0700

    updatehelperrequest

---
+++ app/Http/Requests/UpdateHelperRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateHelperRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'password' => 'min:6',
+            'birthday' => 'required|date_format:Y-m-d|before:today',
+            'phone' => 'required| numeric| digits_between:5,20',
+            'address' => 'required',
+            'gender' => 'required',
+        ];
+    }
+}
