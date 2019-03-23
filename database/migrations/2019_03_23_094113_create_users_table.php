47400de8700e329561262db9bae0ff2fd3f147c7
Happeace304 <happeace304@gmail.com>
2019-03-23 20:48:59 +0700

    Update 2019_03_23_094113_create_users_table.php

--- database/migrations/2019_03_23_094113_create_users_table.php
+++ database/migrations/2019_03_23_094113_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomerTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -36,6 +36,6 @@ class CreateCustomerTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customer');
+        Schema::dropIfExists('users');
     }
 }
