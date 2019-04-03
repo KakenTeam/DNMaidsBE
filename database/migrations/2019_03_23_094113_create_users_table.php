377658872ab4c5f9660d3372b8c94cf5c3530c71
Happeace304 <happeace304@gmail.com>
2019-04-03 09:10:28 +0700

    model groupuser + fix migration user

--- database/migrations/2019_03_23_094113_create_users_table.php
+++ database/migrations/2019_03_23_094113_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('role_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('password');
             $table->string('address')->nullable();
+            $table->string('image')->nullable();
             $table->tinyInteger('gender')->default(0);      //0:nu 1:nam
             $table->date('birthday')->nullable();
             $table->tinyInteger('status')->default(1);                        //0: disable, 1:active
