3c52982ee469a81e0c4a2345ab18f5f532b0a302
Happeace304 <happeace304@gmail.com>
2019-04-05 20:27:45 +0700

    route/api

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,21 @@ Route::get('test', function () {
    return \App\Models\Permission::find(2)->groups;
 });
 Route::group(['namespace' => 'V1\Api'], function () {
-    Route::resource ('users', 'UserController');
 
+    //Authenticate API
     Route::group(['prefix' => 'auth', ], function () {
         Route::post('login', 'AuthController@login');
         Route::post('signup', 'AuthController@signup');
 
-
-
         Route::group(['middleware' => 'auth:api'], function () {
-
             Route::get('logout', 'AuthController@logout');
             Route::get('user', 'AuthController@user');
-
-            Route::group(['middleware' => 'isadmin'], function () {
-                Route::resource ('groups', 'GroupController');
-            });
+        });
+    });
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => 'isadmin'], function () {
+            Route::resource('groups', 'GroupController');      //Groups Management API
+            Route::resource('users', 'UserController');        //Users Management API
         });
     });
 });
