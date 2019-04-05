b85b98bac48ab330174daa18d234840228c46042
Happeace304 <happeace304@gmail.com>
2019-04-05 21:38:02 +0700

    seed group, user

---
+++ database/seeds/GroupsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   $groups = ['Admin', 'User Manage', 'New Group'];
+        foreach ($groups as $group) {
+            \Illuminate\Support\Facades\DB::table('groups')->insert([
+                'group_name' => $group,
+            ]);
+        }
+
+    }
+}
