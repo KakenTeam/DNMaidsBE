c674423a3974dc936888dd95dc91f793a728a14f
Happeace304 <happeace304@gmail.com>
2019-04-10 12:03:24 +0700

    admin seed

---
+++ database/seeds/AdminSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'nhat',
+            'email' => 'admin@gmail.com',
+            'password' => '123456',
+        ]);
+        $admin = \App\Models\User::whereEmail('admin@gmail.com')->first();
+        \Illuminate\Support\Facades\DB::table('group_user')->insert([
+            'user_id' => $admin->id,
+            'group_id' => 1,
+        ]);
+    }
+}
