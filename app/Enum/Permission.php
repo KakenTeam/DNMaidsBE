45ec57ca39575c32571fb68323097ee1a9022643
Happeace304 <happeace304@gmail.com>
2019-04-16 21:53:21 +0700

    update enum

---
+++ app/Enum/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+class Permission {
+
+    public static $ADMIN = "Admin";
+    public static $LIST_USER = "list_user";
+    public static $VIEW_USER = "view_user";
+    public static $CREATE_USER = "create_user";
+    public static $UPDATE_USER = "update_user";
+    public static $DELETE_USER = "delete_user";
+
+}
