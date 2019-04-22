89b3070e9f40123a368b4f8567f75f94d17f6994
Happeace304 <happeace304@gmail.com>
2019-04-22 09:43:53 +0700

    contract seed

---
+++ database/factories/ContractFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\Contract::class, function (Faker $faker) {
+    return [
+        "customer_id" => 1,
+        "helper_id" =>  \App\Models\User::all()->random()->id,
+        "contract_creator_id" =>  \App\Models\User::all()->random()->user_id,
+        "address" => str_random(10),
+	    "start_date" => \Illuminate\Support\Carbon::now()->subDays(random_int(5,10)),
+	    "end_date"=> \Illuminate\Support\Carbon::now()->subDays(random_int(0,5)),
+	    "service_type" => random_int(0,1),
+        "fee" => random_int(200000, 500000),
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GroupsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(AdminSeed::class);
+        $this->call(ContractSeeder::class);
     }
 }
