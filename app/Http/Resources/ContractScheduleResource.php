d7a41c69ff59d08f1380c8210df9e7839dbd87d7
Happeace304 <happeace304@gmail.com>
2019-04-22 16:42:50 +0700

    contract show schedule

---
+++ app/Http/Resources/ContractScheduleResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContractScheduleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "start_time" => $this->start_time,
+            "end_time" => $this->end_time,
+            "day_of_week"=> $this->day_of_week,
+        ];
+    }
+}
