ee4fa26b2f33acede43abcffcef002cd8910b63b
Happeace304 <happeace304@gmail.com>
2019-04-22 10:46:09 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ $factory->define(\App\Models\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'image' => "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjwtMGQ5OLhAhVQQLwKHUTcDeMQjRx6BAgBEAU&url=https%3A%2F%2Fbestie.vn%2F2017%2F07%2Fchu-meo-qua-dang-yeu-va-dep-khong-goc-chet-khien-ai-cung-muon-om-vao-long&psig=AOvVaw0VIM1ag5bn6zCEhNs7Xx9T&ust=1555990950343217",
         'remember_token' => Str::random(10),
     ];
 });
