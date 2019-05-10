98091ebc66a65fcc90f5156f2542b818f48e052a
Happeace304 <happeace304@gmail.com>
2019-05-10 14:17:23 +0700

    Update HelperResource.php

--- app/Http/Resources/HelperResource.php
+++ app/Http/Resources/HelperResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class HelperResource extends JsonResource
 {
@@ -14,13 +15,17 @@ class HelperResource extends JsonResource
      */
     public function toArray($request)
     {
+        $url = null;
+        if ($this->image){
+            $url = Storage::cloud()->url($this->image);
+        }
         return [
             'name' => $this->name,
             'phone' => $this->phone,
             'address' => $this->address,
             'gender' => $this->gender,
             'birthday' => $this->birthday,
-            'image' => $this->image,
+            'image' => $url,
         ];
     }
 }
